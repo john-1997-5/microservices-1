272f759b3dfce99691e5d74db6fb5feea9b6649a
Johnson <johnson.arrobo@gmail.com>
2022-12-26 16:04:20 +0100

    load balancing with openFeign

--- employee-service/src/main/java/com/johnson/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/johnson/employeeservice/service/APIClient.java
@@ -4,11 +4,11 @@ import com.johnson.employeeservice.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 /*
-OpenFeign creará una implementación dinámicamente para esta interfaz
+    APIClient usará internamente el load balancer proporcionado por la dependencia de
+    Eureka Client para llamar a la instancia del micro DEPARTMENT-SERVICE que se encuentre disponible
  */
-@FeignClient(url = "http://localhost:8080", name = "department-service") // base url & nombre micro
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     /*
     aquí declaramos todos los endpoints del otro micro a los que queremos llamar
